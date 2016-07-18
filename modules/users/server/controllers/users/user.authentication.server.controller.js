966965dd9a64b4a90865e2e1f79a832bfcd728a2
Jack Mertens <jmertens@42six-00413.42six.com>
2016-07-18 17:38:15 -0400

    added =

--- modules/users/server/controllers/users/user.authentication.server.controller.js
+++ modules/users/server/controllers/users/user.authentication.server.controller.js
@@ -129,7 +129,7 @@ exports.oauthCallback = function(strategy) {
           return res.redirect('/authentication/signin');
         }
 
-        return res.redirect(typeof redirectURL == 'string' ? redirectURL : sessionRedirectURL || '/');
+        return res.redirect(typeof redirectURL === 'string' ? redirectURL : sessionRedirectURL || '/');
       });
     })(req, res, next);
   };
