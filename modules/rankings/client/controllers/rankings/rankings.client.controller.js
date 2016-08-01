3ba2827c7d67fb9e93932e3ef167a63474bdfec6
Bailey Reed <breed@42six.com>
2016-08-01 13:24:40 -0400

    Implements circuits

--- modules/rankings/client/controllers/rankings/rankings.client.controller.js
+++ modules/rankings/client/controllers/rankings/rankings.client.controller.js
@@ -6,29 +6,42 @@
 
 angular.module('rankings').controller('RankingController', ['$scope', '$filter', 'Rankings',
     function($scope, $filter, Rankings) {
+        $scope.world = [];
+        $scope.major = [];
+        $scope.minor = [];
+        $scope.mosh = [];
 
         Rankings.query(function(data) {
-            console.log("hello from the other side");
-            console.log(data);
             $scope.users = data;
             $scope.buildPager();
         });
 
         $scope.buildPager = function() {
             $scope.pagedItems = [];
-            $scope.itemsPerPage = 15;
+            $scope.itemsPerPage = 100;
             $scope.currentPage = 1;
             $scope.figureOutItemsToDisplay();
         };
 
         $scope.figureOutItemsToDisplay = function() {
-            $scope.filteredItems = $filter('filter')($scope.users, {
+            var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+            var end = begin + $scope.itemsPerPage;
+
+            // Separate out by circuit
+            $scope.world = $scope.filter($scope.users.slice(0, 10));
+            $scope.major = $scope.filter($scope.users.slice(10, 20));
+            $scope.minor = $scope.filter($scope.users.slice(20, 30));
+            $scope.mosh = $scope.filter($scope.users.slice(30, end));
+        };
+
+        $scope.filter = function(users) {
+            $scope.filteredItems = $filter('filter')(users, {
                 $: $scope.search
             });
             $scope.filterLength = $scope.filteredItems.length;
             var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
             var end = begin + $scope.itemsPerPage;
-            $scope.pagedItems = $scope.filteredItems.slice(begin, end);
+            return $scope.filteredItems.slice(begin, end);
         };
 
         $scope.pageChanged = function() {
