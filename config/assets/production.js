18fd57cedbf9daf11fa4c8a781c900873c06a012
Bailey Reed <breed@42six.com>
2016-08-08 15:49:08 -0400

    Production fixes

--- config/assets/production.js
+++ config/assets/production.js
@@ -6,8 +6,15 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/gridster/dist/jquery.gridster.css',
+        'public/lib/decksterjs/dist/jquery.deckster.css',
+        'public/lib/components-font-awesome/css/font-awesome.css'
       ],
       js: [
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/spin.js/spin.js',
+        'public/lib/spin.js/jquery.spin.js',
+        'public/lib/gridster/dist/jquery.gridster.min.js',
         'public/lib/angular/angular.min.js',
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
@@ -16,7 +23,11 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/angular-timeago/dist/angular-timeago.js',
+        'https://www.google.com/recaptcha/api.js',
+        'public/lib/decksterjs/dist/jquery.deckster.js',
+        'public/lib/ng-lodash/build/ng-lodash.js'
       ]
     },
     css: 'public/dist/application.min.css',
