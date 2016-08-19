1531959f97b83b231ebf64ed39a378b54d78889a
Bailey Reed <breed@42six.com>
2016-08-19 14:17:01 -0400

    Fix admin search bar

--- modules/core/client/controllers/displayRoom/drRankingShuffle.client.controller.js
+++ modules/core/client/controllers/displayRoom/drRankingShuffle.client.controller.js
@@ -8,9 +8,6 @@ angular.module('core').controller('DrShuffleController', ['$scope', '$filter', '
     function($scope, $filter, DrRankings, $uibModal, $rootScope, $window) {
 
         $scope.shuffleItems = [];
-        $scope.itemsPerPage = 100;
-        $scope.currentPage = 1;
-        $scope.users = [];
 
         $scope.figureOutItemsToDisplayShuffle = function() {
             $scope.filteredItems = $filter('filter')($scope.users, {
@@ -22,13 +19,25 @@ angular.module('core').controller('DrShuffleController', ['$scope', '$filter', '
             $scope.shuffleItems = $scope.filteredItems.slice(begin, end);
         };
 
+        $scope.buildShufflePager = function() {
+            $scope.pagedItems = [];
+            $scope.itemsPerPage = 100;
+            $scope.currentPage = 1;
+            $scope.figureOutItemsToDisplayShuffle();
+        };
+
         if ($rootScope.displayRoom) {
             DrRankings.query(function(data) {
                 $scope.users = data;
                 $scope.users.sort(function(a,b) {return (a.lastName > b.lastName) ? 1 : ((b.lastName > a.lastName) ? -1 : 0);} );
-                $scope.figureOutItemsToDisplayShuffle();
+                $scope.buildShufflePager();
             });
         }
+
+        $scope.pageChanged = function() {
+            $scope.figureOutItemsToDisplay();
+        };
+
 
         $scope.pageChanged = function() {
             $scope.figureOutItemsToDisplayShuffle();
